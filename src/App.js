aad666d52f0850a3c259bf851766770fffdd426c
vmorandev <vmoran.dev@gmail.com>
2023-01-18 14:43:00 -0500

    updated components to elements

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Auth from "./components/Auth";
 import employeeDashboard from "./pages/employeeDashboard";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import AddEmployeeComponent from "./components/AddEmployeeComponent";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
-      <Router>
-        <h1>I-9 Capstone</h1>
+      <BrowserRouter>
         <Routes>
+          <Route exact path="/" element={<Login />} />
           <Route path="/auth" element={<Auth />} />
-          <Route exact path="/" component={ListEmployeeComponent}></Route>
-          <Route path="/employees" component={ListEmployeeComponent}></Route>
-          <Route path="/add-employee" component={AddEmployeeComponent}></Route>
+          <Route path="/list" element={<ListEmployeeComponent />}></Route>
+          <Route
+            path="/add-employee"
+            element={<AddEmployeeComponent />}
+          ></Route>
           <Route
             path="/edit-employee/:id"
-            component={AddEmployeeComponent}
+            element={<AddEmployeeComponent />}
           ></Route>
           <Route path="/auditor" element={<auditorDashboard />} />
           <Route path="/manager" element={<managerDashboard />} />
           <Route path="/reviewer" element={<reviewerDashboard />} />
-          <Route path="/employee" element={employeeDashboard} />
+          <Route path="/employee" element={<employeeDashboard />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }

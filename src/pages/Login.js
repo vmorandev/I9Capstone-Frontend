e945e79a3dfc6a90501761c6b1c98de16a214039
vmorandev <vmoran.dev@gmail.com>
2023-01-18 14:47:03 -0500

    updated components to elements

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,8 +3,11 @@ import React from "react";
 const Login = () => {
   return (
     <div className="container">
-      <div></div>
-      <h1>Login</h1>
+      <div className="row">
+        <div className="col-md">1 Column</div>
+        <div></div>
+        <h1>Login</h1>
+      </div>
     </div>
   );
 };
